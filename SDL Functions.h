7dae5fb86cb8fb606a774c6d306f07c545eba51a
aaron-d-e <128830108+aaron-d-e@users.noreply.github.com>
2024-11-17 17:14:16 -0600

    Update SDL Functions.h
    collision detection update

--- SDL Functions.h
+++ SDL Functions.h
@@ -11,23 +11,30 @@ const float GRAVITY = .01;
 const float BOUNCINESS = .8;
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 800;
+// BALL CLASS
 class Ball {
 public:
     Ball(float xInitialPos, float yInitialPos, float startXVelo, float startYVelo, float r, SDL_Color c);
-    void Physics(float dT);
-    void renderBall (SDL_Renderer* renderer);
+    void Physics(float dT); //FUNCTION THAT CALCULATES PHYSICS FOR BALL
+    void renderBall (SDL_Renderer* renderer); //FUNCTION THAT RENDERS BALL TO SCREEN
+    float GetX() const { return x; } //GETS X VALUE, *currently unused*
+    float GetY() const { return y; } // GETS Y VALUE, *currently unused*
+    void CircleTriangle1(); //COLLISION FOR TRIANGLE ON RIGHT SIDE OF SCREEN
+    //ADD THIS TO OTHER TRIANGLE, ONLY CHANGE POINT VALUES
 
 private:
     float x, y;
     float xVelocity, yVelocity;
     float radius;
     SDL_Color color;
+    SDL_Point a = {0, 550}, b = {0, 700};
 };
 
+//TRIANGLE CLASS
 class Triangle {
 public:
     Triangle(float xPos, float yPos, float b, float h, SDL_Color c);
-    void renderTriangle(SDL_Renderer* renderer);
+    void renderTriangle(SDL_Renderer* renderer); //RENDERS STATIC TRIANGLES TO SCREEN
 private:
     float base;
     float height;
@@ -39,4 +46,3 @@ private:
 };
 
 #endif //SDL_FUNCTIONS_H
-
