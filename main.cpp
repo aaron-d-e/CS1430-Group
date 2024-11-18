4c9838c5f4751ad4586f602268098211b2e7e381
aaron-d-e <128830108+aaron-d-e@users.noreply.github.com>
2024-11-18 10:58:09 -0600

    Update main.cpp
    added more physics

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char ** argv)
     SDL_Window* window = SDL_CreateWindow("Pinball Game 1.0", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 800, SDL_WINDOW_SHOWN); // window
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED); // renderer
 
-    Ball b1(50, 100, 0.0, 0.0, 25, {255, 0, 0, 255}); // ball object
+    Ball b1(100, 100, 0.0, 0.0, 25, {255, 0, 0, 255}); // ball object
     Triangle t1(0, SCREEN_HEIGHT / 3, 100, 200, {255,255,255,0});
 
     //game loop
@@ -31,21 +31,21 @@ int main(int argc, char ** argv)
 
         PAST = CURRENT;
         CURRENT = SDL_GetPerformanceCounter();
-        deltaTime = ((CURRENT - PAST) * 1000.0) / SDL_GetPerformanceFrequency();
+        deltaTime = ((CURRENT - PAST) * 1000.0) / SDL_GetPerformanceFrequency(); // framerate
 
-
-        b1.Physics(deltaTime);
-        b1.CircleTriangle1();
-
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); //background color
         SDL_RenderClear(renderer);
+
+        b1.Physics(deltaTime); // ball physics
+        b1.TriangleCollision1(deltaTime); // left triangle
+        b1.TriangleCollision2(deltaTime); // right triangle
 
         b1.renderBall(renderer);
 
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0); // color for triangles
         t1.renderTriangle(renderer);
         SDL_RenderPresent(renderer);
+        SDL_RenderClear(renderer);
     }
 
     SDL_DestroyRenderer(renderer);
