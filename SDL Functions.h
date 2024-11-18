0f85fdcbd3368a7e5e96935132d3a94a72ce4e4d
aaron-d-e <128830108+aaron-d-e@users.noreply.github.com>
2024-11-18 10:58:31 -0600

    Update SDL Functions.h
    more physics

--- SDL Functions.h
+++ SDL Functions.h
@@ -14,12 +14,13 @@ const int SCREEN_HEIGHT = 800;
 // BALL CLASS
 class Ball {
 public:
-    Ball(float xInitialPos, float yInitialPos, float startXVelo, float startYVelo, float r, SDL_Color c);
-    void Physics(float dT); //FUNCTION THAT CALCULATES PHYSICS FOR BALL
+    Ball(float xInitialPos, float yInitialPos, float startXVelo, float startYVelo, float r, SDL_Color c); // BALL CONSTRUCTOR
+    void Physics(double dT); //FUNCTION THAT CALCULATES PHYSICS FOR BALL
     void renderBall (SDL_Renderer* renderer); //FUNCTION THAT RENDERS BALL TO SCREEN
     float GetX() const { return x; } //GETS X VALUE, *currently unused*
     float GetY() const { return y; } // GETS Y VALUE, *currently unused*
-    void CircleTriangle1(); //COLLISION FOR TRIANGLE ON RIGHT SIDE OF SCREEN
+    void TriangleCollision1(double dT); //COLLISION FOR TRIANGLE ON LEFT SIDE OF SCREEN
+    void TriangleCollision2(double dT); // COLLISION FOR TRIANGLE ON RIGHT SIDE OF SCREEN
     //ADD THIS TO OTHER TRIANGLE, ONLY CHANGE POINT VALUES
 
 private:
@@ -27,13 +28,14 @@ private:
     float xVelocity, yVelocity;
     float radius;
     SDL_Color color;
-    SDL_Point a = {0, 550}, b = {0, 700};
+    SDL_Point a = {0, 550}, c = {200, 750};
+    SDL_Point d = {799, 550}, e = {599, 750};
 };
 
 //TRIANGLE CLASS
 class Triangle {
 public:
-    Triangle(float xPos, float yPos, float b, float h, SDL_Color c);
+    Triangle(float xPos, float yPos, float b, float h, SDL_Color c); // TRIANGLE CONSTRUCTOR
     void renderTriangle(SDL_Renderer* renderer); //RENDERS STATIC TRIANGLES TO SCREEN
 private:
     float base;
@@ -41,7 +43,7 @@ private:
     float x,y;
     SDL_Color color;
     SDL_Point a = {0, 550}, b = {0, 700}, c = {200, 750};
-    SDL_Point d = {799, 550}, e = {799, 700}, f = {599, 750};
+    SDL_Point d = {799, 550}, e = {599, 750}, f = {799, 700};
 
 };
 
