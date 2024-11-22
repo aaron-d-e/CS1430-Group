185a0dae5b869d4966da2de83af545b22ef675d0
andrew-hutch2 <119772874+andrew-hutch2@users.noreply.github.com>
2024-11-22 11:23:30 -0600

    Create Flipper.h

---
+++ Flipper.h
@@ -0,0 +1,26 @@
+//
+// Created by Andrew Hutcheson on 11/19/2024.
+//
+
+#ifndef SDL2_TEST_FLIPPER_H
+#define SDL2_TEST_FLIPPER_H
+#include <SDL.h>
+#include <cmath>
+
+class Flipper{
+private:
+    float angle;
+    int xPos;
+    int yPos;
+    SDL_Rect box;
+    bool isFlipped = false;
+
+public:
+    bool keydown = false;
+    Flipper(SDL_Renderer *renderer, int x, int y, float angle);
+    void renderFlipper(SDL_Renderer* renderer, SDL_Texture* texture, bool invert);
+    void incrementAngle();
+    void decrementAngle();
+    void collision(SDL_Renderer *renderer);
+};
+#endif //SDL2_TEST_FLIPPER_H
