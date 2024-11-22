17201304750f1b7ad6468f5a3aebd61695697e37
andrew-hutch2 <119772874+andrew-hutch2@users.noreply.github.com>
2024-11-22 11:24:19 -0600

    Update SDL Functions.h

--- SDL Functions.h
+++ SDL Functions.h
@@ -5,23 +5,54 @@
 #ifndef SDL_FUNCTIONS_H
 #define SDL_FUNCTIONS_H
 #include <SDL.h>
-#include "SDL_Plotter.h"
 #include <iostream>
-const float GRAVITY = .01;
-const float BOUNCINESS = .8;
+const double GRAVITY = .0001;
+const double BOUNCINESS = .8;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 800;
+// BALL CLASS
 class Ball {
 public:
-    Ball(float xInitialPos, float yInitialPos, float startXVelo, float startYVelo, float r, SDL_Color c);
-    void Physics(float dT);
-    void render (SDL_Renderer* renderer);
+    Ball(double xInitialPos, double yInitialPos, double startXVelo, double startYVelo, double r, SDL_Color c); // BALL CONSTRUCTOR
+    void Physics(double dT); //FUNCTION THAT CALCULATES PHYSICS FOR BALL
+    void renderBall (SDL_Renderer* renderer); //FUNCTION THAT RENDERS BALL TO SCREEN
+    float GetX() const { return x; } //GETS X VALUE, *currently unused*
+    float GetY() const { return y; } // GETS Y VALUE, *currently unused*
+    float GetYVel() const {return yVelocity;}
+    float GetXVel() const {return xVelocity;}
+    void SetX(float tempX){x = tempX;}
+    void SetY(float tempY){y = tempY;}
+    void SetXVelo(float tempXVelo){xVelocity = tempXVelo;}
+    void SetYVelo(float tempYVelo){yVelocity = tempYVelo;}
+    
+    void TriangleCollision1(double dT); //COLLISION FOR TRIANGLE ON LEFT SIDE OF SCREEN
+    void TriangleCollision2(double dT); // COLLISION FOR TRIANGLE ON RIGHT SIDE OF SCREEN
+    //ADD THIS TO OTHER TRIANGLE, ONLY CHANGE POINT VALUES
 
 private:
-    float x, y;
-    float xVelocity, yVelocity;
-    float radius;
+    double x, y;
+    double xVelocity, yVelocity;
+    double radius;
     SDL_Color color;
+    SDL_Point a = {0, 550}, c = {200, 750};
+    SDL_Point d = {799, 550}, e = {599, 750};
 };
 
+//TRIANGLE CLASS
+class Triangle {
+public:
+    Triangle(double xPos, double yPos, double b, double h, SDL_Color c); // TRIANGLE CONSTRUCTOR
+    void renderTriangle(SDL_Renderer* renderer); //RENDERS STATIC TRIANGLES TO SCREEN
+private:
+    double base;
+    double height;
+    double x,y;
+    SDL_Color color;
+    SDL_Point a = {0, 550}, b = {0, 700}, c = {200, 750};
+    SDL_Point d = {799, 550}, e = {599, 750}, f = {799, 700};
+
+};
+
+void drawCircle (SDL_Renderer* renderer, int x, int y, int radius, SDL_Color c);
+void drawRectangle(SDL_Renderer* renderer, int x, int y, int width, int height, SDL_Color c);
 #endif //SDL_FUNCTIONS_H
-
-
