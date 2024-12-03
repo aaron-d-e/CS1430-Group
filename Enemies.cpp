401a1c9d7daa5055ab9c2629fca07fac22f56e3b
aaron-d-e <128830108+aaron-d-e@users.noreply.github.com>
2024-12-02 20:33:22 -0600

    Update Enemies.cpp

--- Enemies.cpp
+++ Enemies.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "Enemies.h"
+#include <math.h>
+#include "SDL Functions.h"
 
 Enemies::Enemies(double r, double xPos, double yPos, SDL_Color c):
     radius(r), xPosition(xPos), yPosition(yPos), color(c){}
@@ -20,8 +22,26 @@ void Enemies::RenderEnemy(SDL_Renderer *renderer) {
     }
 }
 
-void Enemies::EnemyCollision(double dT) {
-    //FIX ME
+void Enemies::EnemyCollision(double dT, Ball& b, Score& s) {
+    double accuracy = 20;
+    if(sqrt(pow(b.getX() - xPosition, 2) + pow(b.getY() - yPosition, 2)) <= radius + accuracy) {
+    //add one to score
+    }
+
+}
+
+void Enemies::setXPosition(double xPos) {
+    xPosition = xPos;
+}
+void Enemies::setYPostion(double yPos) {
+    yPosition = yPos;
+}
+
+double Enemies::getXPosition()const {
+    return xPosition;
+}
+double Enemies::getYPostion()const {
+    return yPosition;
 }
 
 
