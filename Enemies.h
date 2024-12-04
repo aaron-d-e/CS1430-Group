662ad7efba3553561c65857e7c97c059e2235394
andrew-hutch2 <119772874+andrew-hutch2@users.noreply.github.com>
2024-12-04 17:36:20 -0600

    Update Enemies.h

--- Enemies.h
+++ Enemies.h
@@ -5,24 +5,29 @@
 #ifndef ENEMIES_H
 #define ENEMIES_H
 #include <SDL.h>
-#include "SDL Functions.h"
+#include "SDL_Functions.h"
 
 class Enemies {
 
 public:
-    Enemies(double r, double xPos, double yPos, SDL_Color c); // ENEMIES CONSTRUCTOR
-    void RenderEnemy(SDL_Renderer* renderer); // RENDERS NUMBER OF ENEMIES
+    Enemies(double r, double xPos, double yPos, SDL_Color c, int lives); // ENEMIES CONSTRUCTOR
+    void RenderEnemy(SDL_Renderer* renderer, double dT, Ball& b, Score& s); // RENDERS NUMBER OF ENEMIES
     void EnemyCollision(double dT, Ball& b, Score& s); // FIX ME
+    void respawn(Score &s, int min, int max);
     void setXPosition(double xPos);
-    void setYPostion(double yPos);
+    void setYPosition(double yPos);
     double getXPosition()const;
     double getYPostion()const;
+    int getLives()const;
+    bool isSpawned = true;
 
 private:
     double radius;
+    int lives;
     double xPosition;
     double yPosition;
     SDL_Color color;
+
 };
 
 
