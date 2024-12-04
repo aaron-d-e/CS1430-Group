625b4f3ccb20360b908de1cf2270dd0bbafdab5b
andrew-hutch2 <119772874+andrew-hutch2@users.noreply.github.com>
2024-12-04 17:46:08 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include "Flipper.h"
+#include "LTimer.h"
+#include "Enemies.h"
+//#include <SDL2_TTF.h>
 using namespace std;
 
+//constants for capping frame rate
+const int SCREEN_FPS = 60;
+const int SCREEN_TICKS_PER_FRAME = 1000 / SCREEN_FPS;
+
 int main(int argc, char** argv) {
-    Uint32 CURRENT = SDL_GetPerformanceCounter();
-    Uint32 PAST = 0;
     double deltaTime;
     SDL_Event e;
     SDL_Window* window = SDL_CreateWindow("Pinball Game 1.0", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
@@ -15,7 +20,16 @@ int main(int argc, char** argv) {
     Triangle t1(0, SCREEN_HEIGHT / 3, 100, 200, {255,255,255,0});
 
     //initialize score
-    Score score(0,3, 1.0);
+    Score score(0,10, 1.0);
+
+    //initialize enemies
+    Enemies e1(50, 400, 400, {255,255,255,255}, score.level);
+    Enemies e2(50, 200, 200, {255,255,255,255}, score.level);
+    Enemies e3(50, 600, 200, {255,255,255,255}, score.level);
+    Enemies e4(50, 400, 550, {255,255,255,255}, score.level);
+    e4.isSpawned = false;
+
+
 
     //load texture for Flipper
     SDL_Surface* surface = SDL_LoadBMP("../images/flipper.bmp");
@@ -27,16 +41,30 @@ int main(int argc, char** argv) {
     bool isRunning = true;
     bool userClick = false;
 
+    //stuff for fps calculation
+    LTimer fpsTimer;
+    LTimer capTimer;
+    int countedFrames = 0;
+    fpsTimer.start();
+
     while (isRunning && score.lives > 0) {
+        //framerate cap timer
+        capTimer.start();
+
         //wait for user to click screen and drop ball
         while(!userClick) {
             while (SDL_PollEvent(&e)) {
                 switch (e.type) {
                     case SDL_QUIT:
+                        //if user is trying to quit while in waiting screen
                         isRunning = false;
-                    break;
+                        return 0;
+                        break;
                     case SDL_MOUSEBUTTONDOWN:
                         handleUserClick(userClick, b1, score);
+                        break;
+                    default:
+                        break;
                 }
             }
         }
@@ -46,7 +74,9 @@ int main(int argc, char** argv) {
                     isRunning = false;
                     break;
                 case SDL_MOUSEBUTTONDOWN:
-                    userClick = !userClick;
+                    if (!userClick){
+                        userClick = true;
+                    }
                 case SDL_KEYDOWN:
                     switch (e.key.keysym.sym) {
                         case SDLK_a:
@@ -76,9 +106,21 @@ int main(int argc, char** argv) {
             }
         }
 
-        PAST = CURRENT;
-        CURRENT = SDL_GetPerformanceCounter();
-        deltaTime = ((CURRENT - PAST) * 1000.0) / SDL_GetPerformanceFrequency();
+        //Calculate and correct fps
+        float avgFPS = countedFrames / ( fpsTimer.getTicks() / 1000.f );
+        if( avgFPS > 2000000 )
+        {
+            avgFPS = 0;
+        }
+
+        //If frame finished early
+        int frameTicks = capTimer.getTicks();
+        if( frameTicks < SCREEN_TICKS_PER_FRAME )
+        {
+            //Wait remaining time
+            SDL_Delay( SCREEN_TICKS_PER_FRAME - frameTicks );
+        }
+        deltaTime = SCREEN_TICKS_PER_FRAME;
 
 
         b1.Physics(deltaTime);
@@ -86,40 +128,47 @@ int main(int argc, char** argv) {
         b1.TriangleCollision2(deltaTime);
 
 
-        //rightFlipper.collision(renderer);
-
-
         //update flippers
-        if (leftFlipper.keydown) {
-            leftFlipper.incrementAngle();
-        } else {
-            leftFlipper.decrementAngle();
-        }
-        if (rightFlipper.keydown) {
-            rightFlipper.incrementAngle();
-        } else {
-            rightFlipper.decrementAngle();
-        }
+        leftFlipper.update();
+        rightFlipper.update();
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
         SDL_SetRenderDrawColor(renderer,255,255,255,0);
-        //SDL_RenderDrawRect(renderer, &rect);
+
+
         leftFlipper.renderFlipper(renderer, texture,true);
         rightFlipper.renderFlipper(renderer,texture,false);
-
         leftFlipper.collision(renderer, b1, deltaTime);
         rightFlipper.collision(renderer, b1, deltaTime);
 
-        //score.updateLife(b1);
+
         gamePause(userClick, b1);
+
+        if (e1.isSpawned) {e1.RenderEnemy(renderer, deltaTime, b1, score);}
+        if (e2.isSpawned) {e2.RenderEnemy(renderer, deltaTime, b1, score);}
+        if (e3.isSpawned) {e3.RenderEnemy(renderer, deltaTime, b1, score);}
+        if (e4.isSpawned) {e4.RenderEnemy(renderer, deltaTime, b1, score);}
+        //go to next level
+        if (!e1.isSpawned && !e2.isSpawned && !e3.isSpawned && !e4.isSpawned){
+            score.level += 1;
+            e1.respawn(score, 100, 400);
+            e2.respawn(score, 100, 400);
+            e3.respawn(score, 100, 400);
+            //spawn 4th enemy if higher level
+            if (score.level >= 3){
+                e4.respawn(score, 450, 550);
+            }
+        }
+
 
         b1.renderBall(renderer);
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0); // color for triangles
         t1.renderTriangle(renderer);
 
         SDL_RenderPresent(renderer);
+        ++countedFrames;
     }
 
     SDL_DestroyRenderer(renderer);
