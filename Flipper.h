1fe17e41c18a6f644b7a5572ad1b96b9992d42db
andrew-hutch2 <119772874+andrew-hutch2@users.noreply.github.com>
2024-12-04 17:48:06 -0600

    Update Flipper.h

--- Flipper.h
+++ Flipper.h
@@ -15,6 +15,7 @@ private:
     int yPos;
     SDL_Rect box;
     bool isFlipped = false;
+    bool isMoving = false;
 
 public:
     bool keydown = false;
@@ -22,6 +23,7 @@ public:
     void renderFlipper(SDL_Renderer* renderer, SDL_Texture* texture, bool invert);
     void incrementAngle();
     void decrementAngle();
+    void update();
     void collision(SDL_Renderer *renderer, Ball& ball, double dt);
 };
 #endif //SDL2_TEST_FLIPPER_H
