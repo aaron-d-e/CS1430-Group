3c30a7b6e8e0e0fe9ce62aa30d3eb1ab55c3ff5b
andrew-hutch2 <119772874+andrew-hutch2@users.noreply.github.com>
2024-12-04 17:36:10 -0600

    Update Enemies.cpp

--- Enemies.cpp
+++ Enemies.cpp
@@ -3,13 +3,13 @@
 //
 
 #include "Enemies.h"
-#include <math.h>
-#include "SDL Functions.h"
+#include <cmath>
 
-Enemies::Enemies(double r, double xPos, double yPos, SDL_Color c):
-    radius(r), xPosition(xPos), yPosition(yPos), color(c){}
 
-void Enemies::RenderEnemy(SDL_Renderer *renderer) {
+Enemies::Enemies(double r, double xPos, double yPos, SDL_Color c, int lives):
+        radius(r), xPosition(xPos), yPosition(yPos), color(c), lives(lives){}
+
+void Enemies::RenderEnemy(SDL_Renderer* renderer, double dT, Ball& b, Score& s) {
     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
     for(int i = 0; i < radius * 2; i++) {
         for(int j = 0; j < radius * 2; j++) {
@@ -20,20 +20,46 @@ void Enemies::RenderEnemy(SDL_Renderer *renderer) {
             }
         }
     }
+    double accuracy = 20;
+    if(sqrt(pow(b.getX() - xPosition, 2) + pow(b.getY() - yPosition, 2)) <= radius + accuracy) {
+        //add 10 to score
+        s.addScore(10);
+        lives -= 1;
+        if (lives == 0){
+            isSpawned = false;
+        }
+        b.setXVelo(-b.getXVel());
+        b.setYVelo(-b.getYVel());
+    }
 }
-
+/*
 void Enemies::EnemyCollision(double dT, Ball& b, Score& s) {
     double accuracy = 20;
     if(sqrt(pow(b.getX() - xPosition, 2) + pow(b.getY() - yPosition, 2)) <= radius + accuracy) {
-    //add one to score
+        //add 10 to score
+        s.addScore(10);
+        lives -= 1;
+        if (lives == 0){
+            isSpawned = false;
+        }
+        b.setXVelo(-b.getXVel());
+        b.setYVelo(-b.getYVel());
     }
 
+}*/
+
+void Enemies::respawn(Score &s, int min, int max){
+    lives = s.level;
+    isSpawned = true;
+    //randomize y position
+    int ypos = min + (rand() % (max - min + 1));
+    setYPosition(ypos);
 }
 
 void Enemies::setXPosition(double xPos) {
     xPosition = xPos;
 }
-void Enemies::setYPostion(double yPos) {
+void Enemies::setYPosition(double yPos) {
     yPosition = yPos;
 }
 
@@ -44,5 +70,6 @@ double Enemies::getYPostion()const {
     return yPosition;
 }
 
-
-
+int Enemies::getLives()const{
+    return lives;
+}
