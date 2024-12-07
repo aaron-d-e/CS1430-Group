dbf8777393833fae81c24d893bbbec6d3deaa752
andrew-hutch2 <119772874+andrew-hutch2@users.noreply.github.com>
2024-12-07 17:11:26 -0600

    Update Flipper.h

--- Flipper.h
+++ Flipper.h
@@ -19,11 +19,35 @@ private:
 
 public:
     bool keydown = false;
+    //Description: constructor for FLipper class
+    //Return: none
+    //Precondition: Flipper constructor called correctly
+    //Postcondition: Flipper Class created
     Flipper(SDL_Renderer *renderer, int x, int y, float angle);
+    //Description: Render flipper
+    //Return: none
+    //Precondition: flipper object exists
+    //Postcondition: flipper is rendered to screen
     void renderFlipper(SDL_Renderer* renderer, SDL_Texture* texture, bool invert);
+    //Description: increment flipper angle
+    //Return: none
+    //Precondition: flipper object exists
+    //Postcondition: flipper angle incremented
     void incrementAngle();
+    //Description: decrement flipper angle
+    //Return: none
+    //Precondition: flipper object exists
+    //Postcondition: flipper angle decrement
     void decrementAngle();
+    //Description: update flipper angle
+    //Return: none
+    //Precondition: flipper object exists
+    //Postcondition: flipper angle is updated on screen
     void update();
+    //Description: calculate flipper collision
+    //Return: none
+    //Precondition: flipper object exists
+    //Postcondition: flipper collision calculated
     void collision(SDL_Renderer *renderer, Ball& ball, double dt);
 };
 #endif //SDL2_TEST_FLIPPER_H
