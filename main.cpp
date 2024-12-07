b5b0ee7d44d17d5eb965408cd9d6725bcc64b965
aaron-d-e <128830108+aaron-d-e@users.noreply.github.com>
2024-12-07 17:26:29 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,82 +1,87 @@
 #include "Flipper.h"
-#include "LTimer.h"
 #include "Enemies.h"
-//#include <SDL2_TTF.h>
+#include "LTimer.h"
+#include <ctime>
+#include <SDL_ttf.h>
+#include "BackgroundMusic.h"
+#include "SoundEffect.h"
+#include <iostream>
+#include <charconv>
+
+#include "BackgroundMusic.h"
+
 using namespace std;
 
-//constants for capping frame rate
-const int SCREEN_FPS = 60;
+const int SCREEN_FPS = 160;
 const int SCREEN_TICKS_PER_FRAME = 1000 / SCREEN_FPS;
 
+void displayText(const char* text, Score& s, SDL_Renderer* renderer);
+
 int main(int argc, char** argv) {
+
+    Uint32 CURRENT = SDL_GetPerformanceCounter();
+    Uint32 PAST = 0;
     double deltaTime;
+    bool enemiesspawn = false;
     SDL_Event e;
     SDL_Window* window = SDL_CreateWindow("Pinball Game 1.0", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+
+
+    srand((unsigned)time(0));
+    Uint8 r = (rand() % 255) + 1;
+    Uint8 g = (rand() % 255) + 1;
+    Uint8 b = (rand() % 255) + 1;
+    Uint8 a = (rand() % 255) + 1;
+
+
     //initialize environment
     //BALL INITIAL X and Y are initialized in SDL_Functions.h with other constants
-    Ball b1(BALL_INITIAL_X, BALL_INITIAL_Y, 0.0, -0.1, 20, {255, 0, 0, 255});
+    Ball b1(BALL_INITIAL_X, BALL_INITIAL_Y, 0.1, -0.2, 20, {255, 0, 0, 255});
     Triangle t1(0, SCREEN_HEIGHT / 3, 100, 200, {255,255,255,0});
+    Enemies e1(50, 400, 400, {r,g,b,a});
+    backgroundMusic music("../Assets/Sounds/retro-city-14099.wav", 1);
+
+
 
     //initialize score
     Score score(0,10, 1.0);
 
-    //initialize enemies
-    Enemies e1(50, 400, 400, {255,255,255,255}, score.level);
-    Enemies e2(50, 200, 200, {255,255,255,255}, score.level);
-    Enemies e3(50, 600, 200, {255,255,255,255}, score.level);
-    Enemies e4(50, 400, 550, {255,255,255,255}, score.level);
-    e4.isSpawned = false;
-
-
-
     //load texture for Flipper
-    SDL_Surface* surface = SDL_LoadBMP("../images/flipper.bmp");
+    SDL_Surface* surface = SDL_LoadBMP("../Assets/Sprites/flipper.bmp");
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
     Flipper leftFlipper(renderer, 200,745,0);
     Flipper rightFlipper(renderer, 450, 745,0);
 
     bool isRunning = true;
-    bool userClick = false;
-
-    //stuff for fps calculation
     LTimer fpsTimer;
     LTimer capTimer;
     int countedFrames = 0;
     fpsTimer.start();
 
-    while (isRunning && score.lives > 0) {
-        //framerate cap timer
-        capTimer.start();
+    music.playMusic(100);
 
-        //wait for user to click screen and drop ball
-        while(!userClick) {
-            while (SDL_PollEvent(&e)) {
-                switch (e.type) {
-                    case SDL_QUIT:
-                        //if user is trying to quit while in waiting screen
-                        isRunning = false;
-                        return 0;
-                        break;
-                    case SDL_MOUSEBUTTONDOWN:
-                        handleUserClick(userClick, b1, score);
-                        break;
-                    default:
-                        break;
-                }
-            }
+    while (isRunning && score.lives > 0) {
+        capTimer.start();
+        float avgFPS = countedFrames / ( fpsTimer.getTicks() / 1000.f );
+        if( avgFPS > 2000000 )
+        {
+            avgFPS = 0;
         }
+        int frameTicks = capTimer.getTicks();
+        if( frameTicks < SCREEN_TICKS_PER_FRAME )
+        {
+            //Wait remaining time
+            SDL_Delay( SCREEN_TICKS_PER_FRAME - frameTicks );
+        }
+        deltaTime = SCREEN_TICKS_PER_FRAME - frameTicks;
         while (SDL_PollEvent(&e)) {
             switch (e.type) {
                 case SDL_QUIT:
                     isRunning = false;
                     break;
-                case SDL_MOUSEBUTTONDOWN:
-                    if (!userClick){
-                        userClick = true;
-                    }
                 case SDL_KEYDOWN:
                     switch (e.key.keysym.sym) {
                         case SDLK_a:
@@ -106,61 +111,53 @@ int main(int argc, char** argv) {
             }
         }
 
-        //Calculate and correct fps
-        float avgFPS = countedFrames / ( fpsTimer.getTicks() / 1000.f );
-        if( avgFPS > 2000000 )
-        {
-            avgFPS = 0;
-        }
-
-        //If frame finished early
-        int frameTicks = capTimer.getTicks();
-        if( frameTicks < SCREEN_TICKS_PER_FRAME )
-        {
-            //Wait remaining time
-            SDL_Delay( SCREEN_TICKS_PER_FRAME - frameTicks );
-        }
-        deltaTime = SCREEN_TICKS_PER_FRAME;
-
 
         b1.Physics(deltaTime);
         b1.TriangleCollision1(deltaTime); // left triangle
         b1.TriangleCollision2(deltaTime);
 
 
+        //rightFlipper.collision(renderer);
+
+
         //update flippers
-        leftFlipper.update();
-        rightFlipper.update();
+        if (leftFlipper.keydown) {
+            leftFlipper.incrementAngle();
+        } else {
+            leftFlipper.decrementAngle();
+        }
+        if (rightFlipper.keydown) {
+            rightFlipper.incrementAngle();
+        } else {
+            rightFlipper.decrementAngle();
+        }
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
         SDL_SetRenderDrawColor(renderer,255,255,255,0);
-
-
         leftFlipper.renderFlipper(renderer, texture,true);
         rightFlipper.renderFlipper(renderer,texture,false);
+
         leftFlipper.collision(renderer, b1, deltaTime);
         rightFlipper.collision(renderer, b1, deltaTime);
 
+        score.updateLife(b1);
 
-        gamePause(userClick, b1);
+    displayText("Score", score, renderer);
 
-        if (e1.isSpawned) {e1.RenderEnemy(renderer, deltaTime, b1, score);}
-        if (e2.isSpawned) {e2.RenderEnemy(renderer, deltaTime, b1, score);}
-        if (e3.isSpawned) {e3.RenderEnemy(renderer, deltaTime, b1, score);}
-        if (e4.isSpawned) {e4.RenderEnemy(renderer, deltaTime, b1, score);}
-        //go to next level
-        if (!e1.isSpawned && !e2.isSpawned && !e3.isSpawned && !e4.isSpawned){
-            score.level += 1;
-            e1.respawn(score, 100, 400);
-            e2.respawn(score, 100, 400);
-            e3.respawn(score, 100, 400);
-            //spawn 4th enemy if higher level
-            if (score.level >= 3){
-                e4.respawn(score, 450, 550);
-            }
+
+        if(score.lives <= 10) {
+            e1.RenderEnemy(renderer);
+            e1.EnemyCollision(deltaTime, b1, score, e1);
+            //enemiesspawn = true;
         }
+
+        //e1.EnemyCollision(deltaTime, b1, score, e1);
+        //e2.EnemyCollision(deltaTime, b1, score, e2);
+        //e3.EnemyCollision(deltaTime, b1, score, e3);
+
+
 
 
         b1.renderBall(renderer);
@@ -168,12 +165,37 @@ int main(int argc, char** argv) {
         t1.renderTriangle(renderer);
 
         SDL_RenderPresent(renderer);
-        ++countedFrames;
+        SDL_RenderClear(renderer);
+
+
     }
+
+    //SDL_DestroyTexture(textTexture);
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
+}
+
+void displayText(const char* text, Score& s, SDL_Renderer* rend) {
+    //  font shenanigans
+    if(TTF_Init() < 0) {
+        SDL_Delay(100);
+        exit(1);
+    }
+    TTF_Font* font = TTF_OpenFont("../Assets/Background/ARCADECLASSIC.TTF" ,24);
+    if(!font) {
+        SDL_Delay(100);
+        exit(1);
+    }
+    SDL_Color White = {255,255,255};
+    const char* scoreText = "Score" + s.totalScore;
+    SDL_Surface* textSurface =  TTF_RenderText_Solid(font, scoreText, White);
+    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(rend, textSurface);
+    SDL_FreeSurface(textSurface);
+    SDL_Rect textRect = { 10 ,10 ,400, 100};
+    SDL_RenderCopy(rend, textTexture, NULL, &textRect);
+
 }
