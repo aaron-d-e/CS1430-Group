09aa4af06d703f0cbd08d8bde5e89607252e651c
aaron-d-e <128830108+aaron-d-e@users.noreply.github.com>
2024-12-07 17:29:03 -0600

    Update Enemies.cpp

--- Enemies.cpp
+++ Enemies.cpp
@@ -3,13 +3,19 @@
 //
 
 #include "Enemies.h"
-#include <cmath>
+#include <math.h>
+#include "SDL Functions.h"
+#include "SDL_Plotter.h"
+#include "SoundEffect.h"
+#include <SDL.h>
+#include <SDL_mixer.h>
 
 
-Enemies::Enemies(double r, double xPos, double yPos, SDL_Color c, int lives):
-        radius(r), xPosition(xPos), yPosition(yPos), color(c), lives(lives){}
 
-void Enemies::RenderEnemy(SDL_Renderer* renderer, double dT, Ball& b, Score& s) {
+Enemies::Enemies(double r, double xPos, double yPos, SDL_Color c):
+    radius(r), xPosition(xPos), yPosition(yPos), color(c){}
+
+void Enemies::RenderEnemy(SDL_Renderer *renderer) {
     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
     for(int i = 0; i < radius * 2; i++) {
         for(int j = 0; j < radius * 2; j++) {
@@ -20,40 +26,40 @@ void Enemies::RenderEnemy(SDL_Renderer* renderer, double dT, Ball& b, Score& s) 
             }
         }
     }
-    double accuracy = 20;
-    if(sqrt(pow(b.getX() - xPosition, 2) + pow(b.getY() - yPosition, 2)) <= radius + accuracy) {
-        //add 10 to score
-        s.addScore(10);
-        lives -= 1;
-        if (lives == 0){
-            isSpawned = false;
-        }
-        b.setXVelo(-b.getXVel());
-        b.setYVelo(-b.getYVel());
-    }
 }
-/*
-void Enemies::EnemyCollision(double dT, Ball& b, Score& s) {
+
+void Enemies::EnemyCollision(double dT, Ball& b, Score& s, Enemies& e) {
+
     double accuracy = 20;
-    if(sqrt(pow(b.getX() - xPosition, 2) + pow(b.getY() - yPosition, 2)) <= radius + accuracy) {
-        //add 10 to score
-        s.addScore(10);
-        lives -= 1;
-        if (lives == 0){
-            isSpawned = false;
+    soundEffect sound("../Assets/Sounds/sfx_sounds_interaction1.wav", 1);
+
+        if(sqrt(pow(b.getX() - xPosition, 2) + pow(b.getY() - yPosition, 2)) <= radius + accuracy) {
+            s.addScore(1);
+
+            sound.playSound(125);
+
+
+            b.setXVelo(-b.getXVel());
+            b.setYVelo(-b.getYVel());
+            e.setXPosition(rand()%SCREEN_WIDTH - (radius * 2));
+            e.setYPosition(rand()%SCREEN_HEIGHT);
+
+            e.setCollided(true);
         }
-        b.setXVelo(-b.getXVel());
-        b.setYVelo(-b.getYVel());
+     if(e.getXPosition() < (radius * 2)) {
+         e.setXPosition(e.getXPosition() + 100);
+     }
+    if(e.getYPosition() < (radius * 2)) {
+        e.setYPosition(e.getYPosition() + 100);
+    }
+    if(e.getYPosition() < (radius * 2)) {
+        e.setYPosition(e.getYPosition() + 50);
+    }
+    if(e.getYPosition() > 600) {
+        e.setYPosition(e.getYPosition() - 50);
     }
 
-}*/
 
-void Enemies::respawn(Score &s, int min, int max){
-    lives = s.level;
-    isSpawned = true;
-    //randomize y position
-    int ypos = min + (rand() % (max - min + 1));
-    setYPosition(ypos);
 }
 
 void Enemies::setXPosition(double xPos) {
@@ -62,14 +68,12 @@ void Enemies::setXPosition(double xPos) {
 void Enemies::setYPosition(double yPos) {
     yPosition = yPos;
 }
-
 double Enemies::getXPosition()const {
     return xPosition;
 }
-double Enemies::getYPostion()const {
+double Enemies::getYPosition()const {
     return yPosition;
 }
 
-int Enemies::getLives()const{
-    return lives;
-}
+
+
