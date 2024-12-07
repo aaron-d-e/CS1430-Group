d3d97336c66b11a5e6dafca082f77ee9e3183b7e
aaron-d-e <128830108+aaron-d-e@users.noreply.github.com>
2024-12-07 17:27:45 -0600

    Create SoundEffect.cpp

---
+++ SoundEffect.cpp
@@ -0,0 +1,35 @@
+
+//
+//  SoundEffect.cpp
+//  Sdl_test_please
+//
+//  Created by Jeffrey Rajkumar on 12/2/24.
+//
+
+#include "SoundEffect.h"
+
+soundEffect::soundEffect(): soundPath(), currVolume(), curChunk(){}
+soundEffect::soundEffect(const char* path, double volume){
+    soundPath = path;
+    currVolume = volume;
+    curChunk = 0;
+}
+void soundEffect::setSound(const char* path){
+    soundPath = path;
+}
+void soundEffect::setVolume(int volume){
+    currVolume = volume;
+    Mix_VolumeChunk(curChunk, currVolume);
+}
+string soundEffect::getPath(){
+    return soundPath;
+}
+void soundEffect::playSound(double volume){
+    currVolume = volume;
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024);
+    curChunk = Mix_LoadWAV(soundPath);
+    Mix_VolumeChunk(curChunk, currVolume);
+    Mix_PlayChannel(-1, curChunk, 0);
+
+    
+}
