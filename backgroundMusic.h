32e519372792ff34f2ceca8c669276f846314c45
jrajkumar123 <65698560+jrajkumar123@users.noreply.github.com>
2024-12-08 01:03:43 -0600

    Update backgroundMusic.h

--- backgroundMusic.h
+++ backgroundMusic.h
@@ -18,68 +18,93 @@ private:
     string songName;
     char* songPath;
     int currVolume;
+    Mix_Music* music;
 public:
+    //************************************************************
+    // description: Default constructor for the backgroundMusic class.
+    //              Initializes the object with no music and default
+    //              volume level.
+    // return: None
+    // pre: None
+    // post: A backgroundMusic object is created with no music loaded.
+    //************************************************************
     backgroundMusic();
+
+    //************************************************************
+    // description: Constructor that loads a song from the given path
+    //              and sets the initial volume.
+    // return: None
+    // pre: The file at the provided path must exist and be a valid
+    //      music file.
+    // post: A backgroundMusic object is created with the song loaded.
+    //************************************************************
     backgroundMusic(char* path, double volume);
 
     //************************************************************
-    // description: Sets a new background song from the given path. 
-    // return: None                                               
-    // pre: The file at the provided path must exist and be a valid 
-    //      music file.                                            
-    // post: The current song is replaced with the new song.       
+    // description: Sets a new background song from the given path.
+    // return: None
+    // pre: The file at the provided path must exist and be a valid
+    //      music file.
+    // post: The current song is replaced with the new song.
     //************************************************************
     void setSong(char* path);
 
     //************************************************************
-    // description: Sets the volume level for the background music. 
-    // return: None                                               
-    // pre: Volume must be between 0 and MIX_MAX_VOLUME.          
-    // post: The background music is played at the specified volume. 
+    // description: Sets the volume level for the background music.
+    // return: None
+    // pre: Volume must be between 0 and MIX_MAX_VOLUME.
+    // post: The background music is played at the specified volume.
     //************************************************************
     void setVolume(int volume);
 
     //************************************************************
-    // description: Gets the file path of the current song.       
-    // return: A string containing the file path of the current song. 
-    // pre: None                                                  
-    // post: The file path of the currently loaded song is returned. 
+    // description: Gets the file path of the current song.
+    // return: A string containing the file path of the current song.
+    // pre: None
+    // post: The file path of the currently loaded song is returned.
     //************************************************************
     string getPath();
 
     //************************************************************
-    // description: Halves the current volume of the background music. 
-    // return: None                                                
-    // pre: Volume must be greater than 0 to reduce it.           
-    // post: The volume is halved, ensuring it does not drop below 0. 
+    // description: Halves the current volume of the background music.
+    // return: None
+    // pre: Volume must be greater than 0 to reduce it.
+    // post: The volume is halved, ensuring it does not drop below 0.
     //************************************************************
     void halfVolume();
 
     //************************************************************
-    // description: Mutes the background music.                  
-    // return: None                                              
-    // pre: None                                                 
-    // post: The volume is set to 0.                             
+    // description: Mutes the background music.
+    // return: None
+    // pre: None
+    // post: The volume is set to 0.
     //************************************************************
     void muteVolume();
 
     //************************************************************
-    // description: Unmutes the background music, restoring the   
-    //              volume to its previous level.                 
-    // return: None                                               
-    // pre: The music must have been muted previously.           
-    // post: The volume is restored to the previous level.       
+    // description: Unmutes the background music, restoring the
+    //              volume to its previous level.
+    // return: None
+    // pre: The music must have been muted previously.
+    // post: The volume is restored to the previous level.
     //************************************************************
     void unmuteVolume();
 
     //************************************************************
-    // description: Plays the current background music at the    
-    //              specified volume level.                      
-    // return: None                                              
-    // pre: A valid song must be loaded in songPath.             
-    // post: The background music is played.                    
+    // description: Plays the current background music at the
+    //              specified volume level.
+    // return: None
+    // pre: A valid song must be loaded in songPath.
+    // post: The background music is played.
     //************************************************************
     void playMusic(double volume);
+    //************************************************************
+    // description: Frees the music
+    // return: None
+    // pre: sound should be defined to music
+    // post: frees the music
+    //************************************************************
+    void free();
 };
 
 #endif /* backgroundMusic_h */
