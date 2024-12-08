ebeaf9b0d9a9aefc940e16ebc7ce5a6053a6e526
jrajkumar123 <65698560+jrajkumar123@users.noreply.github.com>
2024-12-08 01:02:54 -0600

    Update SoundEffect.h

--- SoundEffect.h
+++ SoundEffect.h
@@ -20,42 +20,66 @@ private:
     Mix_Chunk* curChunk;
 
 public:
+    //************************************************************
+    // description: Default constructor for the soundEffect class.
+    //              Initializes the object with no sound and
+    //              default volume level.
+    // return: None
+    // pre: None
+    // post: A soundEffect object is created with no sound loaded.
+    //************************************************************
     soundEffect();
+
+    //************************************************************
+    // description: Constructor that loads a sound from the given
+    //              path and sets the initial volume.
+    // return: None
+    // pre: The file at the provided path must exist and be a
+    //      valid sound file.
+    // post: A soundEffect object is created with the sound loaded.
+    //************************************************************
     soundEffect(char* path, double volume);
 
     //************************************************************
-    // description: Sets a new sound effect from the given path.  
-    // return: None                                              
-    // pre: The file at the provided path must exist and be a     
-    //      valid sound file.                                    
-    // post: The current sound is replaced with the new sound.   
+    // description: Sets a new sound effect from the given path.
+    // return: None
+    // pre: The file at the provided path must exist and be a
+    //      valid sound file.
+    // post: The current sound is replaced with the new sound.
     //************************************************************
     void setSound(char* path);
 
     //************************************************************
-    // description: Sets the volume level for the sound effect.  
-    // return: None                                             
-    // pre: Volume must be between 0 and MIX_MAX_VOLUME.        
-    // post: The sound effect is played at the specified volume. 
+    // description: Sets the volume level for the sound effect.
+    // return: None
+    // pre: Volume must be between 0 and MIX_MAX_VOLUME.
+    // post: The sound effect is played at the specified volume.
     //************************************************************
     void setVolume(int volume);
 
     //************************************************************
-    // description: Gets the file path of the current sound effect. 
-    // return: A string containing the file path of the current 
-    //         sound effect.                                      
-    // pre: None                                                 
-    // post: The file path of the currently loaded sound is returned. 
+    // description: Gets the file path of the current sound effect.
+    // return: A string containing the file path of the current
+    //         sound effect.
+    // pre: None
+    // post: The file path of the currently loaded sound is returned.
     //************************************************************
     string getPath();
 
     //************************************************************
-    // description: Plays the currently loaded sound effect.     
-    // return: None                                             
-    // pre: A valid sound must be loaded in curChunk.           
-    // post: The sound effect is played at the set volume.      
+    // description: Plays the currently loaded sound effect.
+    // return: None
+    // pre: A valid sound must be loaded in curChunk.
+    // post: The sound effect is played at the set volume.
     //************************************************************
     void playSound(double volume);
+    //************************************************************
+    // description: Frees the sound
+    // return: None
+    // pre: sound should be defined to curChunk
+    // post: frees the chunk
+    //************************************************************
+    void free();
 };
 
 #endif /* SoundEffect_h */
