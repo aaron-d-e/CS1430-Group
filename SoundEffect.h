55c839c4ac0ab60938942006ee298b460e8ee6b8
jrajkumar123 <65698560+jrajkumar123@users.noreply.github.com>
2024-12-08 00:39:18 -0600

    Update SoundEffect.h

--- SoundEffect.h
+++ SoundEffect.h
@@ -5,29 +5,85 @@
 //  Created by Jeffrey Rajkumar on 12/2/24.
 //
 
-#ifndef SoundEffect_h
-#define SoundEffect_h
-#include <iostream>
-#include <SDL2/SDL.h>
-#include <SDL2_mixer/SDL_mixer.h>
-#include <SDL2_image/SDL_image.h>
-using namespace std;
+#ifndef SOUNDEFFECT_H
+#define SOUNDEFFECT_H
 
-class soundEffect {
+#include <string>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+
+/**
+ * @class SoundEffect
+ * @brief A class to manage and play sound effects using the SDL2 library.
+ */
+class SoundEffect {
 private:
-    char* soundPath;
-    int currVolume;
-    Mix_Chunk* curChunk;
+    std::string soundPath; ///< Path to the sound file.
+    int currVolume;        ///< Current volume level (0 to MIX_MAX_VOLUME).
+    Mix_Chunk* curChunk;   ///< Pointer to the loaded sound effect.
+
 public:
-    soundEffect();
-    soundEffect(char* path, double volume);
-    void setSound(char* path);
+    //************************************************************
+    // description: Default constructor for the SoundEffect class. 
+    //              Initializes the object with no sound and a 
+    //              default volume level.                         
+    // return: None                                               
+    // pre: None                                                  
+    // post: An empty SoundEffect object is created.              
+    //************************************************************
+    SoundEffect();
+
+    //************************************************************
+    // description: Constructor that loads a sound from a given path 
+    //              and sets the initial volume.                   
+    // return: None                                                
+    // pre: The file at the provided path must exist and be a valid 
+    //      sound file.                                            
+    // post: A SoundEffect object with the sound loaded is created. 
+    //************************************************************
+    SoundEffect(const std::string& path, int volume);
+
+    //************************************************************
+    // description: Destructor for the SoundEffect class. 
+    //              Releases resources associated with the sound. 
+    // return: None                                              
+    // pre: None                                                 
+    // post: All allocated resources are released.               
+    //************************************************************
+    ~SoundEffect();
+
+    //************************************************************
+    // description: Sets a new sound effect from a given path. 
+    // return: None                                            
+    // pre: The file at the provided path must exist and be a 
+    //      valid sound file.                                  
+    // post: The current sound is replaced with the new sound. 
+    //************************************************************
+    void setSound(const std::string& path);
+
+    //************************************************************
+    // description: Sets the volume level for the sound effect. 
+    // return: None                                            
+    // pre: Volume must be between 0 and MIX_MAX_VOLUME.       
+    // post: The sound effect is played at the specified volume. 
+    //************************************************************
     void setVolume(int volume);
-    string getPath();
-    void playSound(double volume);
-    
-    
+
+    //************************************************************
+    // description: Gets the file path of the current sound effect. 
+    // return: A string containing the file path.                  
+    // pre: None                                                   
+    // post: The path of the currently loaded sound is returned.   
+    //************************************************************
+    std::string getPath() const;
+
+    //************************************************************
+    // description: Plays the currently loaded sound effect.     
+    // return: None                                            
+    // pre: A valid sound must be loaded in curChunk.           
+    // post: The sound effect is played at the set volume.      
+    //************************************************************
+    void playSound();
 };
 
-
-#endif /* SoundEffect_h */
+#endif /* SOUNDEFFECT_H */
